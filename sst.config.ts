7f823b1cf435d5a6fcb7851550648a510ea9f9a5
Rómulo Ochoa Valez <romulo.ochoa@worldcoo.com>
2024-09-28 12:27:20 +0200

    Init

--- sst.config.ts
+++ sst.config.ts
@@ -3,17 +3,21 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "sst-queue-subscription-issue",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
   },
   async run() {
-    await import("./infra/storage");
-    const api = await import("./infra/api");
+    // If the lambda has no changes it will not be updated, as expected...
+    new sst.aws.Function("Lambda", { handler: "src/lambda.handler" });
 
-    return {
-      api: api.myApi.url,
-    };
+    const topic = new sst.aws.SnsTopic("Topic");
+    const queue = new sst.aws.Queue("Queue");
+    queue.subscribe("src/handle-queue.handler");
+
+    // ...but this subscription will always be updated even if there are no changes
+    // to it or its resources
+    topic.subscribeQueue(queue.arn);
   },
 });
